466a49f079cc24e0950ab4aeecfd9d7441450c9e
JavierVivas <javier.vivas@icloud.com>
2026-03-25 16:29:53 +0100

    style: fix order middlewares

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import { authRoutes } from './src/routes/auth.routes.js';
 import { usersRoutes } from './src/routes/users.routes.js';
 const app = express();
 
 //Middlewares
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
+app.use(morgan('dev'));
 
 //Routes
 authRoutes(app);
